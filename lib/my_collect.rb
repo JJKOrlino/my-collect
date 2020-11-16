128985e7421e7eca6b5c1017c1f78565519209d8
JJKOrlino <jjkorlino80@yahoo.com>
2020-11-15 19:03:21 -0600

    commit tree 5da522c675aa7bbc43cfb52ddd9fa8bf7ee0e2f2

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(empty_array)
+    i = 0
+    students = []
+        while i < empty_array.length
+            students.push yield empty_array[i]
+            i += 1
+        end
+        students
+    end
 
-
+# my_collect(students) do |student|
+#     name.split (" ").first
